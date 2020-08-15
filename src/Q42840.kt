7c27e6960840e35ccc06baab9d85afe908bc286f
roy <roy.kim@goodoc.co.kr>
2020-08-15 23:31:38 +0900

    Init Commit

---
+++ src/Q42840.kt
@@ -0,0 +1,38 @@
+import kotlin.test.assertTrue
+
+fun main(args: Array<String>) {
+
+    val main = Q42840()
+    assertTrue { intArrayOf(1).contentEquals(main.solution(intArrayOf(1, 2, 3, 4, 5))) }
+    assertTrue { intArrayOf(1, 2, 3).contentEquals(main.solution(intArrayOf(1, 3, 2, 4, 2))) }
+}
+
+class Q42840 {
+    fun solution(answers: IntArray): IntArray {
+        val pattern1 = intArrayOf(1, 2, 3, 4, 5)
+        val pattern2 = intArrayOf(2, 1, 2, 3, 2, 4, 2, 5)
+        val pattern3 = intArrayOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
+        val studentPatterns = arrayOf(pattern1, pattern2, pattern3)
+
+        var maxAnswer = 0
+        val winners = arrayListOf<Int>()
+
+        for ((studentIndex, studentAnswer) in studentPatterns.withIndex()) {
+            val correctAnswer = answers.filterIndexed { answerIndex, answer ->
+                answer == studentAnswer[answerIndex % studentAnswer.size]
+            }.count()
+
+            val studentNumber = studentIndex + 1
+            if (correctAnswer > maxAnswer) {
+                winners.clear()
+                winners.add(studentNumber)
+                maxAnswer = correctAnswer
+
+            } else if (correctAnswer == maxAnswer) {
+                winners.add(studentNumber)
+            }
+        }
+
+        return winners.toIntArray()
+    }
+}
---
+++ src/Q64061.kt
@@ -0,0 +1,60 @@
+import java.util.*
+import kotlin.test.assertEquals
+
+fun main(args: Array<String>) {
+//        [[0, 0, 0, 0, 0], [0, 0, 1, 0, 3], [0, 2, 5, 0, 1], [4, 2, 4, 4, 2], [3, 5, 1, 3, 1]], [1, 5, 3, 5, 1, 2, 1, 4]
+    val board1 = intArrayOf(0, 0, 0, 0, 0)
+    val board2 = intArrayOf(0, 0, 1, 0, 3)
+    val board3 = intArrayOf(0, 2, 5, 0, 1)
+    val board4 = intArrayOf(4, 2, 4, 4, 2)
+    val board5 = intArrayOf(3, 5, 1, 3, 1)
+    val board = arrayOf(board1, board2, board3, board4, board5)
+
+    val moves = intArrayOf(1, 5, 3, 5, 1, 2, 1, 4)
+
+    assertEquals(4, Q64061().solution(board, moves))
+}
+
+class Q64061 {
+
+    fun solution(board: Array<IntArray>, moves: IntArray): Int {
+        var answer = 0
+        val stack = Stack<Int>()
+
+        for (move in moves) {
+            val popValue = pop(board, move)
+
+            if (stack.isEmpty()) {
+                stack.push(popValue)
+                continue
+            }
+
+            if (popValue != 0) {
+                if (popValue == stack.peek()) {
+                    stack.pop()
+                    answer += 2
+                } else {
+                    stack.push(popValue)
+                }
+            }
+        }
+
+        return answer
+    }
+
+    private fun pop(board: Array<IntArray>, move: Int): Int {
+        val index = move - 1
+
+        var popValue = 0
+        for (array in board) {
+            if (array[index] != 0) {
+                popValue = array[index]
+                array[index] = 0
+                break
+            }
+        }
+
+        return popValue
+    }
+
+}
