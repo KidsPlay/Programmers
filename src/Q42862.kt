8c3df812c2974c8e1e33e977ea3a2cc417644095
roy <roy.kim@goodoc.co.kr>
2020-08-22 22:56:07 +0900

    체육복 문제 완료.

--- src/Q42862.kt
+++ src/Q42862.kt
@@ -3,30 +3,30 @@ import kotlin.test.assertEquals
 fun main(args: Array<String>) {
     val main = Q42862()
     assertEquals(5, main.solution(5, intArrayOf(2, 4), intArrayOf(1, 3, 5)))
+    assertEquals(5, main.solution(5, intArrayOf(2, 4), intArrayOf(1, 3, 4, 5)))
+    assertEquals(4, main.solution(5, intArrayOf(2, 3), intArrayOf(5, 4, 3)))
     assertEquals(4, main.solution(5, intArrayOf(2, 4), intArrayOf(3)))
     assertEquals(2, main.solution(3, intArrayOf(3), intArrayOf(1)))
+    assertEquals(2, main.solution(2, intArrayOf(3), intArrayOf(3)))
 }
 
 class Q42862 {
 
     fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
-        var answer = n - lost.size
 
-        var reserveIndex = 0
-        run loop@{
-            lost.forEach { lostNum ->
-                if (reserve[reserveIndex] == (lostNum - 1) || reserve[reserveIndex] == (lostNum + 1) || reserve[reserveIndex] == lostNum) {
-                    answer++
-                    reserveIndex++
-                }
+        var answer = n
+        val lostSet = lost.toSet() - reserve.toSet()
+        val reserveSet = (reserve.toSet() - lost.toSet()) as MutableSet
 
-                if (reserveIndex >= reserve.size) {
-                    return@loop
-                }
+        for (i in lostSet) {
+            when {
+                i + 1 in reserveSet -> reserveSet.remove(i + 1)
+                i - 1 in reserveSet -> reserveSet.remove(i - 1)
+                else -> answer--
             }
         }
 
         return answer
-    }
 
+    }
 }
