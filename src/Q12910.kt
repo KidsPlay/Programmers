7934cd4b2e4fbaed6404d82b1c377e2cfc2696b9
roy <roy.kim@goodoc.co.kr>
2020-09-02 23:08:51 +0900

    [나누어 떨어지는 숫자 배열] 문제 완료!

---
+++ src/Q12910.kt
@@ -0,0 +1,22 @@
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+fun main(args: Array<String>) {
+    val main = Q12910()
+
+    assertTrue { intArrayOf(5, 10).contentEquals(main.solution(intArrayOf(5, 9, 7, 10), 5)) }
+    assertTrue { intArrayOf(1, 2, 3, 36).contentEquals(main.solution(intArrayOf(2, 36, 1, 3), 1)) }
+    assertTrue { intArrayOf(-1).contentEquals(main.solution(intArrayOf(3, 2, 6), 10)) }
+}
+
+class Q12910 {
+    fun solution(arr: IntArray, divisor: Int): IntArray {
+        arr.filter { it % divisor == 0 }.toIntArray().let {
+            return if (it.isNotEmpty()) {
+                it.sortedArray()
+            } else {
+                intArrayOf(-1)
+            }
+        }
+    }
+}
