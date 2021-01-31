f549e33387601e30c945d8aac7b0fcf2bc380218
roy <roy.kim@goodoc.co.kr>
2021-01-31 20:42:28 +0900

    62947 - Java 구현 완료.

---
+++ src/lessons_10302/Q62947_J.java
@@ -0,0 +1,31 @@
+package lessons_10302;
+
+import org.junit.Assert;
+
+import java.util.Arrays;
+
+public class Q62947_J {
+    public static void main(String[] args) {
+        Q62947_J main = new Q62947_J();
+
+        Assert.assertEquals(3, main.solution(new int[]{5, 1, 3, 7}, new int[]{2, 2, 6, 8}));
+        Assert.assertEquals(0, main.solution(new int[]{2, 2, 2, 2}, new int[]{1, 1, 1, 1}));
+    }
+
+    public int solution(int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int aIndex = 0;
+        int answer = 0;
+
+        for (int b : B) {
+            if (A[aIndex] < b) {
+                answer++;
+                aIndex++;
+            }
+        }
+
+        return answer;
+    }
+}
