a9f2bb417b158e5ccedb0b44156346dc37b83d52
roy <roy.kim@goodoc.co.kr>
2021-02-02 21:48:18 +0900

    42862 - Java 구현 완료.

--- src/level1/Q42862_J.java
+++ src/level1/Q42862_J.java
@@ -2,68 +2,70 @@ package level1;
 
 import org.junit.Assert;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-
 public class Q42862_J {
     public static void main(String[] args) {
         Q42862_J main = new Q42862_J();
 
-//        Assert.assertEquals(5, main.solution(5, new int[]{2, 4}, new int[]{1, 3, 5}));
-//        Assert.assertEquals(4, main.solution(5, new int[]{2, 4}, new int[]{3}));
-//        Assert.assertEquals(2, main.solution(3, new int[]{3}, new int[]{1}));
-        Assert.assertEquals(3, main.solution(5, new int[]{1,2,4,5}, new int[]{2,3,4}));
+        Assert.assertEquals(5, main.solution(5, new int[]{2, 4}, new int[]{1, 3, 5}));
+        Assert.assertEquals(4, main.solution(5, new int[]{2, 4}, new int[]{3}));
+        Assert.assertEquals(2, main.solution(3, new int[]{3}, new int[]{1}));
+        Assert.assertEquals(3, main.solution(5, new int[]{1, 2, 4, 5}, new int[]{2, 3, 4}));
     }
 
     public int solution(int n, int[] lost, int[] reserve) {
-//        int answer = n;
-//
-//        for (int lostNum : lost) {
-//            boolean isRent = false;
-//
-//            for (int j = 0; j < reserve.length; j++) {
-//                if (Math.abs(lostNum - reserve[j]) <= 1) {
-//                    reserve[j] = -1;
-//                    isRent = true;
-//                    break;
-//                }
-//            }
-//
-//            if (!isRent) {
-//                answer--;
-//            }
-//        }
-//
-//        return answer;
+        int[] students = new int[n];
+        int answer = n;
 
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
+        for (int l : lost) {
+            students[l - 1]--;
+        }
 
-        int answer = 0;
-        int lIndex = 0;
-        int rIndex = 0;
+        for (int r : reserve) {
+            students[r - 1]++;
+        }
 
-        for (int sNum = 1; sNum <= n; sNum++) {
-            if (lIndex >= lost.length || sNum != lost[lIndex]) {
-                answer++;
-
-            } else {
-                for (; rIndex < reserve.length; rIndex++) {
-                    if (Math.abs(sNum - reserve[rIndex]) <= 1) {
-                        answer++;
-                        rIndex++;
-                        break;
-
-                    } else if (sNum < reserve[rIndex]) {
-                        break;
-                    }
+        //안가져온 자리는 -1, 추가로 가져온 자리는 1
+        for (int i = 0; i < students.length; i++) {
+            if (students[i] == -1) {
+                if (i > 1 && students[i - 1] > 0) {
+                    students[i]++;
+                    students[i - 1]--;
+                } else if (i < students.length - 1 && students[i + 1] > 0) {
+                    students[i]++;
+                    students[i + 1]--;
+                } else {
+                    answer--;
                 }
-
-                lIndex++;
             }
         }
 
         return answer;
     }
+
+//    public int solution(int n, int[] lost, int[] reserve) {
+//        Set<Integer> lostSet = new HashSet<>();
+//        Set<Integer> tempLostSet = new HashSet<>();
+//        for (int l : lost) {
+//            lostSet.add(l);
+//            tempLostSet.add(l);
+//        }
+//
+//        Set<Integer> reserveSet = new HashSet<>();
+//        for (int r : reserve) {
+//            reserveSet.add(r);
+//        }
+//
+//        lostSet.removeAll(reserveSet);
+//        reserveSet.removeAll(tempLostSet);
+//
+//        int answer = n;
+//
+//        for (int l : lostSet) {
+//            if (!reserveSet.remove(l - 1) && !reserveSet.remove(l + 1)) {
+//                answer--;
+//            }
+//        }
+//
+//        return answer;
+//    }
 }
