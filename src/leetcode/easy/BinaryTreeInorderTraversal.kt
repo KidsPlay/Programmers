b771e6004ae7083b6010126d1959242e93485764
roy <roy.kim@goodoc.co.kr>
2021-06-10 22:04:00 +0900

    LeetCode BinaryTreeInorderTraversal 완료

---
+++ src/leetcode/easy/BinaryTreeInorderTraversal.kt
@@ -0,0 +1,30 @@
+package leetcode.easy
+
+import org.junit.Assert
+import kotlin.test.assertEquals
+
+fun main() {
+    Assert.assertEquals(listOf(1, 3, 2), inorderTraversal(TreeNode(1).apply {
+        right = TreeNode(2).apply {
+            left = TreeNode(3)
+        }
+    }))
+
+    assertEquals(emptyList(), inorderTraversal(null))
+    assertEquals(listOf(1), inorderTraversal(TreeNode(1)))
+}
+
+private fun inorderTraversal(root: TreeNode?): List<Int> {
+    val list = mutableListOf<Int>()
+    inorder(root, list)
+
+    return list
+}
+
+fun inorder(node: TreeNode?, list: MutableList<Int>) {
+    if (node != null) {
+        inorder(node.left, list)
+        list.add(node.`val`)
+        inorder(node.right, list)
+    }
+}
